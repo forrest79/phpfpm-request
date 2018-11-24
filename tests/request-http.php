a6b71d90c000a241744bf4e6da6fb773fe92fb7c
Jakub Trmota <jakub.trmota@forrest79.net>
2018-11-24 16:33:39 +0100

    Travis: enable php-fpm and install libfcgi0ldbl, update tests

--- tests/request-http.php
+++ tests/request-http.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+header_remove();
+
 header('Header-one: test1');
 header('header-two: test2');
 
--- tests/request-text.php
+++ tests/request-text.php
@@ -1,3 +1,5 @@
 <?php declare(strict_types=1);
 
+header_remove();
+
 echo 'OK-TEXT';
